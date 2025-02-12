fe5f1e6fbe6b0599fc11174f2f329974be61c16b
rhtajik <144002592+rhtajik@users.noreply.github.com>
2025-02-12 23:35:28 +0100

    oprette en ClassRoom  og students

--- ClassRoomNet60/Program.cs
+++ ClassRoomNet60/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ClassRoomNet60;
+
+class Program
+{
+    static void Main()
+    {
+        ClassRoom myClass = new ClassRoom
+        {
+            ClassName = "3Q",
+            SemesterStart = new DateTime(2019, 8, 26)
+        };
+
+
+        myClass.StudentList.Add(new Student("Kasper Larsen", 5, 15));
+        myClass.StudentList.Add(new Student("Lars Hasen", 3, 22));
+        myClass.StudentList.Add(new Student("Sofie Jensen", 7, 8));
+
+
+        Console.WriteLine($"Class Name: {myClass.ClassName}");
+        Console.WriteLine($"Semester Start: {myClass.SemesterStart.ToShortTimeString()}");
+        Console.WriteLine("Students:");
+        foreach (var student  in myClass.StudentList)
+        {
+            Console.WriteLine($"  {student.Name},       Born:   {student.BirthMonth}/{student.Birthday}");
+        }
+
+
+    }
+
+}
--- ClassRoomNet60/Student.cs
+++ ClassRoomNet60/Student.cs
@@ -21,5 +21,8 @@ namespace ClassRoomNet60
             BirthMonth = birthMonth;
             Birthday = birthday;
         }
+
+
+
     }
 }
